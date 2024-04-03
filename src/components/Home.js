74c8f77114ebbc8d8d754fc44715c6320c3098d2
piochod <piochod@wp.pl>
2024-04-03 11:17:50 +0200

    minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
 
       <div className='right'>
           <h1>Welcome !</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a orci dolor. Vivamus laoreet leo quis dui bibendum, sed tempus diam maximus. Integer ac sollicitudin lectus. Sed porta aliquam dictum. Nulla sit amet lectus quis tortor scelerisque malesuada a vitae risus. Nulla sit amet lectus quis tortor scelerisque malesuada a vitae risus. Sed posuere, neque ac placerat pretium, lectus tortor bibendum ex, ut suscipit diam metus in quam.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a orci dolor. Vivamus laoreet leo quis dui bibendum, sed tempus diam maximus. Integer ac sollicitudin lectus. Sed porta aliquam dictum. Nulla sit amet lectus quis tortor scelerisque malesuada a vitae risus.</p>
 
       </div>
     </div>
